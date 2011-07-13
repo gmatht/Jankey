827e41d25aa29f6d5c3f8be5cc29a2eed488e1bd
John C. McCabe-Dansted <gmatht@gmail.com>
2011-07-14 00:08:18 +0800

    Include whole output of /bin/time in output.

--- examples/Benchmarks.sh
+++ examples/Benchmarks.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+#rm Benchmarks.out
 for v in `cd lyx.cache ; echo 2???? 3????`
+#for v in `cd lyx.cache ; echo 30120 3{4,5,6,7,8,9}???`
 do
-	examples/Benchmark.sh lyx.cache/$v_bin/bin/lyx
-	echo $v `head -n1 /tmp/lyx-code.txt.err` 8192=`grep asdf /tmp/lyx-code.txt.lyx | wc -l`
-done | tee Benchmarks.out
+	rm /tmp/lyx-code.txt.*
+	examples/Benchmark.sh lyx.cache/${v}_bin/bin/lyx
+	echo $v `tr '\n' ' ' < /tmp/lyx-code.txt.time` 8192=`grep asdf /tmp/lyx-code.txt.lyx | wc -l` >> Benchmarks.out
+done #| tee Benchmarks.out
 	
