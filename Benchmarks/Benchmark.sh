e3f9b9569e0f9d191e221590f1d367e922f2ad69
John C. McCabe-Dansted <gmatht@gmail.com>
2011-07-30 16:05:36 +0800

    Change what we are benchmarking.

--- Benchmarks/Benchmark.sh
+++ Benchmarks/Benchmark.sh
@@ -3,15 +3,10 @@
 # Run as: 
 # KT=/mnt/big/xp/images/share/Ubuntu904/keytest
 # ( sudo -H -u keytest2 ./kt; DISPLAY=:3 xwininfo -root || nice -19 ./initXvfb 3; DISPLAY=:3 nice -19 ./cache-bisect.py sudo -H -u keytest2 $KT/doNtimes.sh 3 $KT/WrongLine.sh )  2>&1 | tee tmpfs/bisect4.log
-#
-#
-#WL=/home/xp/svn/PhD/t2/WrongLine3
-#src/lyx $WL/t2.lyx $WL/t0.lyx &
-#WINDOW_NAME=WrongLine.sh.t2.lyx
-
-#set -x
 
 TMPFILE=/tmp/lyx-$USER.txt
+mkdir -p /tmp/bak-$USER
+mv $TMPFILE*  /tmp/bak-$USER
 WINDOW_NAME="LyX: ..."
 if [ -z "$1" ]
 then
@@ -31,21 +26,18 @@ cd `dirname $0`
 echo NO_CLIP | xclip 
 #NEW_HOME=`mktemp`
 echo PWD `pwd`
-#HOME=$NEW_HOME $EXE $0.t2.lyx $0.t0.lyx &
-#`pwd`_bin/bin/lyx $0.t2.lyx $0.t0.lyx &
-#$EXE 2> $TMPFILE.err > $TMPFILE & #./WrongLine.sh.t2.lyx ./WrongLine.sh.t0.lyx &
 pwd
 echo "time -o $TMPFILE.time $EXE 2> $TMPFILE.err  &" #./WrongLine.sh.t2.lyx ./WrongLine.sh.t0.lyx &
 rm $TMPFILE.lyx
 rm $TMPFILE.lyx.emergency
 
 /usr/bin/time -o $TMPFILE.time  $EXE $TMPFILE.lyx 2> $TMPFILE.err  &#./WrongLine.sh.t2.lyx ./WrongLine.sh.t0.lyx &
-#(time $EXE 2> $TMPFILE.err)  & #./WrongLine.sh.t2.lyx ./WrongLine.sh.t0.lyx &
+#valgrind --tool=massif $EXE $TMPFILE.lyx 2> $TMPFILE.err  &#./WrongLine.sh.t2.lyx ./WrongLine.sh.t0.lyx &
+ps  -o pid,ppid
+echo LYX_PID="ps  -o pid,ppid | grep \ $!\$  | sed s/\ .*//" # Get child of time process
 LYX_PID=`ps  -o pid,ppid | grep \ $!  | sed s/\ .*//` # Get child of time process
 echo .LYX_PID=$LYX_PID.
 echo NO_CLIP | xclip 
-#while ! (wmctrl -F -R LyX || wmctrl -F -R lyx)
-#while ! (wmctrl -R "LyX: $TMPFILE.lyx")
 i=0
 while ! (wmctrl -R "LyX")
 do
@@ -63,7 +55,9 @@ done
 sleep 0.1
 #for K in `echo '\Af n \Cs . . } \As RR \Ao RR \CD \[Escape] \Cq'`
 #for K in `echo '\Cn \D9 \Ai \D9  n \D9  n \D9 nnn \D9 \[Right] asdf \D9 \r \r iadsf \Ca \Cc \Cv \Cv  \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc MEMINFO \Cq \Cs \Ad \Ad'`
-for K in `echo '\Ac \D1 \Ac \D9 \D9 \Ao \D1\Av\D10 \Ai n n nnn \[Right] asdf \r \r iadsf \Ao \Ca \D9 \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Cc MEMINFO \Cq \Cs \Ad \Ad'`
+#for K in `echo '\Ac \D1 \Ac \D9 \D9 \Ao \D1\Av\D10 \Ai n n nnn \[Right] asdf \r \r iadsf \Ao \Ca \D9 \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Ca \Ca \Cc \Cv \Cv \Ca \Cc \Cv \Cv \Cc MEMINFO \Cq \Cs \Ad \Ad'`
+#for K in `echo '\Ac \D1 \Ac \D9 \D9 \Ao  \D1\Av\D101 \b \b \e \D1 a \D1 \Cm \D1  b \D9  \[Right] \[Right] c' ; for i in $(seq 1 8) ; do echo ' \Ca \Cc \Cv  \Cv ' ; done ; for i in $(seq 1 80) ; do echo -n ' \[Left] ' ; done ;  echo 'MEMINFO \S\C\[End] \Cc \Cq \Cs \Ad \Ad'`
+for K in `echo '\Ac \D1 \Ac \D9 \D9 \Ao  \D1\Av\D101 \b \b \e \D1 a \D1 \Cm \D1  b \D9  \[Right] \[Right] ccc' ; for i in $(seq 1 8) ; do echo ' \Ca \Cc \Cv  \Cv ' ; done ; for i in $(seq 1 200) ; do echo -n ' \[Left] \[\Right] \[Left] \[Right] \[Right] ' ; done ;  echo 'MEMINFO \S\C\[End] \Cc \Cq \Cs \Ad \Ad'`
 do
 	if [ "$K" = RR ]
 	then
@@ -74,13 +68,26 @@ do
 	elif [ "$K" = MEMINFO ]
 	then
 		ps gaux | grep _bin/bin/lyx | grep -v grep
-		echo CLIP_LEN `xclip -o -selection CLIPBOARD | wc -l`
+		echo CLIP_CONT `xclip -o -selection CLIPBOARD` | tee > $TMPFILE.xclipc
+		echo CLIP_LEN `xclip -o -selection CLIPBOARD | wc` | tee > $TMPFILE.xclip
 	else
+		i=0
 		while test -e "/proc/$LYX_PID/status" && ! grep 'S .sleeping.' "/proc/$LYX_PID/status"
 		do
 			echo -n s
-			sleep 0.1
+			sleep 0.05
+
+			i=$(($i+1))
+			if [ "$i" -gt 1000 ]
+			then
+				kill $LYX_PID
+				sleep 1
+				kill -9 $LYX_PID
+				exit
+			fi
+
 		done
+		sleep 0.05
 
 			
 
@@ -109,10 +116,6 @@ done
 wmctrl -R "LyX: Save" ;sleep 0.3; xvkbd -xsendevent -text '\Ad'; sleep 0.3
 wmctrl -R desktop; sleep 0.3
 wmctrl -R "LyX: Save" ;sleep 0.3; xvkbd -xsendevent -text '\Ad'; sleep 0.3
-#wmctrl -R "LyX: Save" ;sleep 0.3; xvkbd -xsendevent -text '\Ad'; sleep 0.3
-#wmctrl -R "LyX: Save" ;sleep 0.3; xvkbd -xsendevent -text '\Ad'; sleep 0.3
-#wmctrl -R "LyX: Save" ;sleep 0.3; xvkbd -xsendevent -text '\Ad'; sleep 0.3
-#wmctrl -R "LyX: Save" ;sleep 0.3; xvkbd -xsendevent -text '\Ad'; sleep 0.3
 #xvkbd -xsendevent -text '\Cs\D9\D9.\D9\D9c\D9\D9\Ad\D9\D9\D9s\D9\D9\Ac\D9\D9\AD\D9\D9\D9\D9\D9\[Escape]\D9\D9\[Escape]'
 #sleep 2
 sleep 0.3
@@ -121,10 +124,4 @@ sleep 0.3
 kill -9 $LYX_PID
 sleep 0.3 ; kill $! ; sleep 0.3 ; kill -9 $!  &
 cat $TMPFILE.err
-#if grep "terminate called without an active exception" < $TMPFILE.err 
-#then
-#	exit 1
-#else
-#	exit 0
-#fi
 #exit 125 # Not good or bad, just ugly.
--- Benchmarks/Benchmarks.sh
+++ Benchmarks/Benchmarks.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #rm Benchmarks.out
-#for v in `cd lyx.cache ; echo 2???? 3????`
-for v in `cd lyx.cache ; echo 3816?`
+
+if [ -z "VERS" ]
+then
+	VERS="`cd lyx.cache ; echo 2???? 3????`"
+fi
+
+for v in $VERS
+#for v in `cd lyx.cache ; echo 20010`
 do
-	rm /tmp/lyx-$USER.txt.*
-	examples/Benchmark.sh lyx.cache/${v}_bin/bin/lyx
-	echo $v `tr '\n' ' ' < /tmp/lyx-$USER.txt.time` 8192=`grep asdf /tmp/lyx-$USER.txt.lyx | wc -l` >> Benchmarks.out
+	#rm -rf Benchmark_copy_dir
+	mkdir -p Benchmarks_copy_dir
+	mv /tmp/lyx-$USER.txt.* Benchmarks_copy_dir/
+	Benchmarks/Benchmark.sh lyx.cache/${v}_bin/bin/lyx
+	#echo $v `tr '\n' ' ' < /tmp/lyx-$USER.txt.time` 8192=`grep asdf /tmp/lyx-$USER.txt.lyx | wc -l` >> Benchmarks.out
+	echo $v `tr '\n' ' ' < /tmp/lyx-$USER.txt.time` 8192=`cat /tmp/lyx-$USER.txt.xclip | tr -d '\n'` >> Benchmarks.out
 done #| tee Benchmarks.out
 	
