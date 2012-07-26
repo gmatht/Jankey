88baaeabb7d538b672bef9505fd917a701ba0819
John C. McCabe-Dansted <gmatht@gmail.com>
2012-07-26 17:47:45 +0800

    Add some logging

--- cache-bisect-all.sh
+++ cache-bisect-all.sh
@@ -1,5 +1,8 @@
 export ANY_CRASH="y"
+rm cache-bisect-all.log
 for f in "$@" 
 do
+	echo S "$f" > cache-bisect-all.log
 	./timeout3.sh -t 36000 -i 5 -d 20 ./cache-bisect.sh "$f"
+	echo E "$f" > cache-bisect-all.log
 done
